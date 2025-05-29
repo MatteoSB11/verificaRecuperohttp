852a0c41a00cdc44595e31b91fd5acda70299341
MatteoSB11 jgjg <matteosbarra07@gmail.com>
2025-05-29 08:45:37 +0000

    Punto 3 non funzionante

--- sbarra-canzoniere/src/app/app.component.ts
+++ sbarra-canzoniere/src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Canzoni } from './models/song.model';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +11,30 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'verifica';
+  @Input() canzone! : Canzoni
+  http! : HttpClient
+  obs! : Observable <object>
+  data! : object
+  loading! : boolean
+  vettCanzoni : Canzoni[] = []
+
+  construcor(http : HttpClient){
+    this.http = http
+  }
+  Richiesta(){
+    this.loading = true
+    this.obs = this.http.get('https://my-json-server.typicode.com/malizia-g/hotel/short-songlist')
+    this.obs.subscribe(this.getData)
+  }
+  getData = (d : object) => {
+    this.data = d
+    this.loading = false
+    console.log(this.data)
+  }
+  ngOnInit(): void {
+    this.Richiesta()
+  }
+  
 }
